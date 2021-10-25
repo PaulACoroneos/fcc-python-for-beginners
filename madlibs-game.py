ef2fef7edce1bf3d601e81b3bea043494b21363d
Paul Coroneos <paul@pcoroneos.com>
2021-10-24 23:45:49 -0500

    madlibs

---
+++ madlibs-game.py
@@ -0,0 +1,8 @@
+color =input("Choose a color: ")
+plural_noun =input("Choose a plural noun: ")
+celebrity =input("Choose a celebrity: ")
+
+print("Rose are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
+
