fb52f7c05e416765ec26967a801abbae6d47bddc
Paul Coroneos <paul@pcoroneos.com>
2021-10-24 23:57:04 -0500

    lists2

--- lists.py
+++ lists.py
@@ -1,4 +1,5 @@
-friends = ["zach","robert"]
+friends = ["zach", "robert"]
+lucky_numbers = [4, 8, 15, 16, 23, 42]
 
 print(friends)
 print(friends[0])
@@ -6,3 +7,18 @@ print(friends[-1])
 print(friends[1:])
 friends[1] = "lolipop"
 print(friends)
+friends.append("Creed")
+print(friends.sort())
+print(friends.reverse())
+friends.extend(lucky_numbers)
+friends.insert(1,"Pam")
+friends.remove("zach")
+print(friends.pop())
+print(friends.index("Pam"))
+friends.clear()
+friends.insert(1,"Pam")
+friends.insert(1,"Pam")
+friends.insert(1,"Pam")
+print(friends.count("Pam"))
+friends2 = friends.copy()
+print(friends2)
