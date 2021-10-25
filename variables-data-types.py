dd9eec410e8bb46ad49cc44684e96ffc8c189f14
Paul Coroneos <paul@pcoroneos.com>
2021-10-24 22:46:03 -0500

    complete variables

---
+++ variables-data-types.py
@@ -0,0 +1,11 @@
+character_name = "John"
+character_age = 50
+is_mail = True
+
+print("There once was a man named " + character_name + ", ")
+print('He was 70 years old')
+
+character_name = "Mike"
+
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age)
