4edad1a359a84f77ef69b7692802af087a70659d
Paul Coroneos <paul@pcoroneos.com>
2021-10-24 23:18:27 -0500

    finish working w/ strings

---
+++ working-with-strings.py
@@ -0,0 +1,18 @@
+print("Giraffe Academy")
+print("Giraffe\"Academy")
+print("Giraffe\nAcademy")
+
+phrase = "Giraffe Academy"
+
+print(phrase)
+print(phrase + " is cool")
+print(phrase.lower())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("G"))
+print(phrase.index("a"))
+print(phrase.index("Acad"))
+# print(phrase.index("z"))
+print(phrase.replace("Giraffe", "Elephant"))
