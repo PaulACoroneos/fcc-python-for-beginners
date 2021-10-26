cb4b415e2d19fcab15860187c1a14e78f95a16fb
Paul Coroneos <paul@pcoroneos.com>
2021-10-25 21:55:18 -0500

    class and object

---
+++ classes-objects.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self, name, major, gpa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
+
+
+student = Student("Paul", "Computer Engineering", 3.5, False)
+
