e180f723d8e6a03e3a3f718d5f8bd75f1f9452bf
Paul Coroneos <paul@pcoroneos.com>
2021-10-25 22:09:24 -0500

    class functions

---
+++ class_function.py
@@ -0,0 +1,5 @@
+from classes_objects import Student
+
+student = Student("Paul", "Computer Engineering", 3.5, False)
+
+print(student.honors_roll(),student.name)
--- classes_objects.py
+++ classes_objects.py
@@ -4,7 +4,9 @@ class Student:
         self.major = major
         self.gpa = gpa
         self.is_on_probation = is_on_probation
+    def honors_roll(self):
+      return self.gpa >= 3.5
 
 
-student = Student("Paul", "Computer Engineering", 3.5, False)
+# student = Student("Paul", "Computer Engineering", 3.5, False)
 
