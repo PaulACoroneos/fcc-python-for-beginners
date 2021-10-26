bb4a06362ed499fcd9a7e5bd929ff46e9dfd0476
Paul Coroneos <paul@pcoroneos.com>
2021-10-25 20:21:52 -0500

    if statements

---
+++ if.py
@@ -0,0 +1,9 @@
+is_male = True
+is_tall = False
+
+if is_male and is_tall:
+  print("You are a male or tall")
+elif is_male and not is_tall:
+  print("You are male but not tall")
+else:
+  print("You are not male nor tall")
