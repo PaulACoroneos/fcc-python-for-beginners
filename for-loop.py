a193ada4496e9e696f2f9c1088661bffcb51e280
Paul Coroneos <paul@pcoroneos.com>
2021-10-25 21:02:20 -0500

    for loop

---
+++ for-loop.py
@@ -0,0 +1,16 @@
+friends = ["zach","allen"]
+
+for letter in "llamas are cool":
+  print(letter)
+
+for friend in friends:
+  print(friend)
+
+for index in range(10):
+  print(index)
+
+for index in range(3,10):
+  print(index)
+
+for index in range(len(friends)):
+  print(friends[index])
